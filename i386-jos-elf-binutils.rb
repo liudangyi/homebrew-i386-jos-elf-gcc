29d2f381a312bad83b756d70645a77ca8345e789
jorge <jorge@jorges-MacBook-Pro.local>
2017-12-12 07:07:22 -0500

    ready

--- i386-jos-elf-binutils.rb
+++ i386-jos-elf-binutils.rb
@@ -1,7 +1,7 @@
 class I386JosElfBinutils < Formula
   homepage "http://pdos.csail.mit.edu/6.828/2014/tools.html"
   url "http://ftpmirror.gnu.org/binutils/binutils-2.21.1.tar.bz2"
-  sha1 "525255ca6874b872540c9967a1d26acfbc7c8230"
+  sha256 "525255ca6874b872540c9967a1d26acfbc7c8230"
 
   def install
     system "./configure", "--prefix=#{prefix}",
